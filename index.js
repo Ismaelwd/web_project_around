08dc74797e50769962c0ad60ee7cb324f7fbc2f3
IsmaelHdz <ismaelwd7@gmail.com>
2025-02-17 10:40:39 -0800

    Editar CSS

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const openPopup = document.querySelector(".popup");
-const buttonPopup = document.querySelector(".profile__button-info");
+const buttonPopup = document.querySelector(".profile__info-button_info");
 const buttonClosePopup = document.querySelector(".popup__button-close");
+const formElement = document.querySelector(".popup__form");
+const nameInput = document.querySelector(".popup__form-name");
+const jobInput = document.querySelector(".popup__form-occupation");
+const displayName = document.querySelector(".profile__info-name");
+const displayDescription = document.querySelector(".profile__info-description");
+const buttonSubmit = document.querySelector(".popup__form-button");
+
+nameInput.value = "Jacques Cousteau";
+jobInput.value = "Explorador";
 
 buttonPopup.addEventListener("click", () => {
   openPopup.classList.add("opened");
@@ -10,16 +19,11 @@ buttonClosePopup.addEventListener("click", () => {
   openPopup.classList.remove("opened");
 });
 
-let formElement = document.querySelector(".popup__form");
-let nameInput = formElement.querySelector(".popup__form-name");
-let jobInput = formElement.querySelector(".popup__form-occupation");
-let buttonSubmit = formElement.querySelector(".popup__form-button");
-
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  let nameInput = formElement.querySelectorAll(".popup__form-name");
-  let jobInput = formElement.querySelectorAll(".popup__form-occupation");
-  console.log(nameInput);
+  displayName.textContent = nameInput.value;
+  displayDescription.textContent = jobInput.value;
+  openPopup.classList.remove("opened");
 }
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
